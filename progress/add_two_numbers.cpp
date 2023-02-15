af7c09505eaf83ee41f242e57fde1636bf182d8a
Jcoddin1 <jcoddin1@binghamton.edu>
2023-02-15 17:38:11 -0500

    LL initial template added

--- progress/add_two_numbers.cpp
+++ progress/add_two_numbers.cpp
@@ -1,3 +1,4 @@
+//Initial template from GFG
 #include <iostream>
 using namespace std;
 
@@ -11,7 +12,7 @@ class Node {
             next = NULL;
         }
 
-        Noe(int data){
+        Node(int data){
             this->data = data;
             this->next = NULL;
         }
@@ -28,7 +29,7 @@ class Linkedlist{
         void insertNode(int);
         void printList();
         void deleteNode(int);
-}
+};
 
 void Linkedlist::deleteNode(int nodeOffset){
     Node *temp1 = head, *temp2 = NULL;
@@ -37,5 +38,80 @@ void Linkedlist::deleteNode(int nodeOffset){
         return;
         // List is empty
     }
+
+    while (temp1!=NULL){
+        temp1 = temp1->next;
+        ListLen++;
+    }
     
+    if(ListLen<nodeOffset) {
+        cout << "Index out of range" << endl;
+        return;
+    }
+
+    temp1 = head;
+
+    if(nodeOffset == 1){
+        head = head->next;
+        delete temp1;
+        return;
+    }
+
+    while (nodeOffset-- >1){
+        temp2 = temp1;
+        temp1=temp1->next;
+        temp2->next = temp1->next;
+    }
+
+    temp2->next = temp1->next;
+
+    delete temp1;
+}
+
+void Linkedlist::insertNode (int data){
+    Node *newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+
+void Linkedlist::printList()
+{
+    Node* temp = head;
+
+    if(head == NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    while (temp != NULL) {
+         cout << temp->data << " ";
+         temp = temp->next;
+    }
+}
+
+int main(){
+    Linkedlist list;
+
+    list.insertNode(1);
+    list.insertNode(2);
+    list.insertNode(3);
+    list.insertNode(4);
+    cout << "Elements of the list are: ";
+    list.printList();
+    cout << endl;
+    list.deleteNode(2);
+
+    cout << "Elements of the list are ";
+    list.printList();
+    cout << endl;
+    return 0;
 }
