fcb06f286fbf7bd04fc52759c18c825bfadd6ce3
Jcoddin1 <jcoddin1@binghamton.edu>
2024-11-01 16:39:51 -0400

    resub lc

--- problems/prob1957.cpp
+++ problems/prob1957.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        //int i=2;
-        if(s.size()>2){
-            for(int i=2;i<=s.size();i++){
-                if(s[i]==s[i-1]&&s[i]==s[i-2]){
-                    s.erase(i,1);
+        int count=1;
+        string answer = "";
+        answer.push_back(s[0]);
+            for(int i=1;i<s.size();i++){
+                if(s[i] == answer.back()){
+                    count++;
+                    if(count<3) { answer.push_back(s[i]);}
+                }
+                else{
+                    count = 1;
+                    answer.push_back(s[i]);
                 }
             }
-        }
-    return s;
-	}
+        return answer;
+    }
+// Uses solution by Jayant Singla to solve runtime issue
 };
